558858936d3d8360ff0773166d8087fd4458e111
kecifej <67418021+kecifej@users.noreply.github.com>
2021-10-17 20:50:02 +0200

    Update index.php

--- index.php
+++ index.php
@@ -101,7 +101,7 @@
 				<div class="divTableBody">
 					<div class="divTableRow" style="height: 520px;">
 						<div id="product-video" class="divTableCellInfo" style="background: rgba(0,0,0,1);">
-							<div style="width: 100%; height: 0px; position: relative; padding-bottom: 62.500%;"><iframe src="https://streamable.com/e/uz4ndy" frameborder="0" width="100%" height="100%" allowfullscreen style="width: 100%; height: 100%; position: absolute;"></iframe></div>
+							<div style="width: 100%; height: auto; position: relative; padding-bottom: 62.500%;"><iframe src="https://streamable.com/e/uz4ndy" frameborder="0" width="100%" height="100%" allowfullscreen style="width: 100%; height: 100%; position: absolute;"></iframe></div>
 						</div>
 					</div>
 				</div>
